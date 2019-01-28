9fe99754c9af2b0d229306b28616fbad893c3e86
Guilherme P. Lima <gui.pedrosa@gmail.com>
2019-01-28 18:34:17 -0200

    set heroku env

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,3 +1,9 @@
 export const settings = {
-    restApi: 'http://localhost:3000/api'
+    restApi: function() {
+        if (process.env.NODE_ENV == 'production'){
+            return 'https://webfolio-backend.herokuapp.com/api'
+        } else {
+            return 'http://localhost:3000/api'
+        }
+    }
 }
