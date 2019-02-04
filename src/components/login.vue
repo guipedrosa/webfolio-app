7d7b807bf899960ff886553f9395fbba85449199
Guilherme P. Lima <gui.pedrosa@gmail.com>
2019-02-04 20:41:18 -0200

    last fixes

--- src/components/login.vue
+++ src/components/login.vue
@@ -120,9 +120,12 @@ export default {
             this.$router.push({ path: '/dashboard' })
           })
           .catch(err => {
-            if (err.response.status == 403) {
+            if (err.response && err.response.status == 403) {
               this.message_login = this.$t('wrong_user_or_password')
               this.showDismissibleAlert = true
+            } else {
+              this.message_login = this.$t('problem_to_login')
+              this.showDismissibleAlert = true    
             }
           })
   
--- src/components/quiz.vue
+++ src/components/quiz.vue
@@ -1,10 +1,37 @@
 <template>
-    <h2>Quiz</h2>
+    <div>
+        <h2>Quiz</h2>
+
+        <b-card no-body>
+            <b-tabs pills card vertical end>
+                <b-tab title="#1" active>
+                    <b-form-group label="Pergunta sobre o caso 1, escolha uma resposta!">
+                        <b-form-radio-group buttons stacked button-variant="primary" v-model="selected" name="butons1" :options="options">
+                        </b-form-radio-group>
+                    </b-form-group>                 
+                    <b-button type="submit" variant="success">Responder</b-button>
+                </b-tab>
+            </b-tabs>
+        </b-card>
+
+        <br>
+        <p>Você já respondeu 2/6 <b-button type="submit" variant="primary" disabled>Finalizar</b-button></p>
+    </div>
 </template>
 
 <script>
 export default {
-    
+  data () {
+    return {
+      selected: null, // Must be an array reference!
+      options: [
+        {text: 'Orange', value: 'orange'},
+        {text: 'Apple', value: 'apple'},
+        {text: 'Pineapple', value: 'pineapple'},
+        {text: 'Grape', value: 'grape'}
+      ]
+    }
+  }
 }
 </script>
 
