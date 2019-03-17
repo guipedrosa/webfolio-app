de65761af72397b26e07f6a0c069a3ec7112a2fb
Guilherme P. Lima <gui.pedrosa@gmail.com>
2019-03-16 23:36:10 -0300

    service worker registration and creation

--- src/main.js
+++ src/main.js
@@ -94,6 +94,22 @@ new Vue({
 }).$mount('#app')
 
 
+/* 
+ * Service Worker
+ */
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/serviceWorker.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+
 window.fbAsyncInit = function() {
   FB.init({
     appId      : '431103770762225',
